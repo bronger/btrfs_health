8d1021e6238ee2862db83a74551bb150b644f1ff
Torsten Bronger <bronger@physik.rwth-aachen.de>
2020-06-01 02:53:04 +0200

    Added get_scrub_results.

--- read_status.py
+++ read_status.py
@@ -48,4 +48,28 @@ def get_errors(filesystems):
                 match = re.match(r"\[(?P<device>.+)\]\..+_errs\s+(?P<errors>\d+)", line)
                 devices[path] = devices.get(path, 0) + int(match.group("errors"))
     return devices
+
+
+def get_scrub_results(filesystems):
+    devices = {}
+    for data in filesystems.values():
+        for device in data["devices"].values():
+            path = device["path"] 
+            output =  subprocess.run(["btrfs", "scrub", "start", "-B", path], check=True, capture_output=True,
+                                     text=True).stdout
+            match = re.match(r"""scrub device (?P<path>.+) \(id (?P<devid>\d+)\) done
+Scrub started:\s* (?P<timestamp>.+)
+Status:\s* finished
+Duration:\s* (?P<duration>.+)
+Total to scrub:\s* (?P<total_to_scrub>.+)
+Rate:\s* (?P<rate>.+)
+Error summary:\s* no errors found
+""", output, re.MULTILINE)
+            if match:
+                devices[path] = {"timestamp": match.group("timestamp"),
+                                 "duration": match.group("duration"),
+                                 "total_to_scrub": match.group("total_to_scrub"),
+                                 "rate": match.group("rate")}
+            else:
+                devices[path] = {"error_message": output}
     return devices
