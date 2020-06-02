db0237e4b8199541950f934e8484ff99b8b2a1e4
Torsten Bronger <bronger@physik.rwth-aachen.de>
2020-06-02 10:01:01 +0200

    Added docstrings.

--- read_status.py
+++ read_status.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
 
+"""Helper routines for converting btrfs’ output into something processable.  In
+particular, it fullfills the need to decide whethr a btrfs filesystem is
+healthy or not.  Unfortunately, btrfs’ userland utilities produce output that
+is unstable in ordering, unstable in device naming, complex to parse, and not
+accompanied by proper exit codes.
+"""
+
+
 import re, subprocess
 
 
 def get_filesystems():
+    """Returns the “present filesystems” as btrfs calls them.  I suspect, *mounted*
+    filesystems are meant.
+
+    :returns:
+      All mounted btrfs filesystems, as a dictionary mapping the UUID to
+      filesystem data.  The filesystem data is a dictionary mapping field names
+      to its value.  The fields are “label” (UUID), “number_devices”,
+      “bytes_used”, and “devices”.  For the field “devices”, the value is a
+      dictionary mapping the “devid” (a number) to a dictionary mapping field
+      names to device data.  Here, the field names are “size”, “used”, and
+      “path” (e.g. “/dev/sda”).
+
+    :rtype: dict[str, dict[str, object]]
+    """
     btrfs = subprocess.run(["btrfs", "fi", "show"], check=True, capture_output=True, text=True)
     assert not btrfs.stderr, btrfs.stderr
     lines = iter(btrfs.stdout.splitlines())
@@ -39,6 +61,20 @@ def get_filesystems():
 
 
 def get_errors(filesystems):
+    """Returns filesystem errors detected by “btrfs device stats”.  This call is
+    rather fast (< one second).
+
+    :param filesystems: the filesystems to be checked, as returned by
+      `get_filesystems`.
+
+    :type filesystems: dict[str, dict[str, object]]
+
+    :returns:
+      The device status as a dictionary mapping the device path
+      (e.g. “/dev/sda” to the number of recorded errors.
+
+    :rtype: dict[str, int]
+    """
     devices = {}
     for data in filesystems.values():
         for device in data["devices"].values():
@@ -51,6 +87,22 @@ def get_errors(filesystems):
 
 
 def get_scrub_results(filesystems):
+    """Returns filesystem errors detected by “btrfs device stats”.  This call is
+    expensive (scrubbing of all devices).
+
+    :param filesystems: the filesystems to be checked, as returned by
+      `get_filesystems`.
+
+    :type filesystems: dict[str, dict[str, object]]
+
+    :returns:
+      The device status as a dictionary mapping the device path
+      (e.g. “/dev/sda”) to a dictionary mapping field names to values.  In case
+      of errors, the full scrub error message is returned in the single field
+      “error_message”.
+
+    :rtype: dict[str, dict[str, str]]
+    """
     devices = {}
     for data in filesystems.values():
         for device in data["devices"].values():
